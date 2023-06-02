761142e5b11688564e78eff4c668105894f5d49f
Springhawk27 <sajjad0mahmud@gmail.com>
2023-06-02 21:50:26 +0600

    basic logger setup done

--- src/server.ts
+++ src/server.ts
@@ -1,17 +1,20 @@
-/* eslint-disable no-console */
 import mongoose from 'mongoose'
 import app from './app'
 import config from './config/index'
+import logger from './shared/logger'
 
 async function bootstrap() {
   try {
     await mongoose.connect(config.database_url as string)
-    console.log(`Database Connected Successfully`)
+    // console.log(`Database Connected Successfully`)
+    logger.info(`Database Connected Successfully`)
     app.listen(config.port, () => {
-      console.log(`Application listening on port ${config.port}`)
+      // console.log(`Application listening on port ${config.port}`)
+      logger.info(`Application listening on port ${config.port}`)
     })
   } catch (err) {
-    console.log(`Database Connection Failed`, err)
+    // console.log(`Database Connection Failed`, err)
+    logger.error(`Database Connection Failed`, err)
   }
 }
 
