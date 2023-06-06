52be8e4e860af04a28e3fdf35ec5e7b9f8a16c99
Springhawk27 <sajjad0mahmud@gmail.com>
2023-06-06 19:14:07 +0600

    different ways of error handling tried

--- src/app.ts
+++ src/app.ts
@@ -11,20 +11,51 @@ app.use(cors())
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
 
-// eslint-disable-next-line no-console
 loggerInfo.info(app.get('env'))
 
 // Application Routes
 app.use('/api/v1/users/', usersRouter)
 
-// testing
+// class ApiError extends Error {
+//   statusCode: number
+//   constructor(statusCode: number, message: string | undefined, stack = '') {
+//     super(message)
+//     this.statusCode = statusCode
+//     if (stack) {
+//       this.stack = stack
+//     } else {
+//       Error.captureStackTrace(this, this.constructor)
+//     }
+//   }
+// }
+
 app.get('/', async (req: Request, res: Response) => {
-  // await usersService.createUser({
-  //   id: '999',
-  //   password: '1234',
-  //   role: 'student',
-  // })
   res.send('Working Successfully!')
 })
+
+// testing
+// eslint-disable-next-line no-unused-vars
+// app.get('/', async (req: Request, res: Response, next: NextFunction) => {
+//   // await usersService.createUser({
+//   //   id: '999',
+//   //   password: '1234',
+//   //   role: 'student',
+//   // })
+//   res.send('Working Successfully!')
+//   // throw new ApiError(400, 'Error Error Errror')
+//   // next('Error Error Errror2')
+//   // throw new Error('Error Error Errror')
+// })
+
+// global error handler
+// eslint-disable-next-line no-unused-vars
+// app.use((err, req: Request, res: Response, next: NextFunction) => {
+//   // console.log(err)
+//   if (err instanceof Error) {
+//     res.status(400).json({ error: err })
+//   } else {
+//     res.status(500).json({ error: 'something went wrong' })
+//   }
+// })
 
 export default app
