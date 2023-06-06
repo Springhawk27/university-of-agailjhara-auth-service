1a8a429161b3b901d1926cfac1e9cf05aa6c5db4
Springhawk27 <sajjad0mahmud@gmail.com>
2023-06-06 22:24:41 +0600

    common error pattern for front end implemented

--- src/app/middlewares/globalErrorHandler.ts
+++ src/app/middlewares/globalErrorHandler.ts
@@ -1,4 +1,7 @@
+/* eslint-disable no-unused-vars */
 import { NextFunction, Request, Response } from 'express'
+import { IGenericErrorMessage } from '../../interfaces/error'
+import config from './../../config'
 
 const globalErrorHandler = (
   err: any,
@@ -6,8 +9,19 @@ const globalErrorHandler = (
   res: Response,
   next: NextFunction
 ) => {
-  res.status(400).json({ err: err })
-  next()
+  // res.status(400).json({ err: err })
+  // next()
+
+  const statusCode = 500
+  const message = 'Something Went Wrong'
+  const errorMessages: IGenericErrorMessage[] = []
+
+  res.status(statusCode).json({
+    success: false,
+    message,
+    errorMessages,
+    stack: config.env !== 'production' ? err?.stack : undefined,
+  })
 }
 
 export default globalErrorHandler
