1c42d113eb2b7f79f18f74b08d85b62f85a90345
Springhawk27 <sajjad0mahmud@gmail.com>
2023-06-08 02:53:01 +0600

    fixed error log and server off implemented

--- src/app/middlewares/globalErrorHandler.ts
+++ src/app/middlewares/globalErrorHandler.ts
@@ -1,14 +1,21 @@
+/* eslint-disable no-unused-expressions */
 /* eslint-disable no-unused-vars */
 import { ErrorRequestHandler } from 'express'
 import config from '../../config'
 import ApiError from '../../errors/ApiErrors'
 import handleValidationError from '../../errors/handleValidationError'
 import { IGenericErrorMessage } from '../../interfaces/error'
+import { loggerError } from '../../shared/logger'
 
 // if the error is the first parameter then this is a error path pattern hence - ErrorRequestHandler
 const globalErrorHandler: ErrorRequestHandler = (error, req, res, next) => {
   // res.status(400).json({ err: err })
   // next()
+
+  config.env === 'development'
+    ? // eslint-disable-next-line no-console
+      console.log('globalErrorHandler', error)
+    : loggerError.error('globalErrorHandler', error)
 
   let statusCode = 500
   let message = 'Something Went Wrong'
