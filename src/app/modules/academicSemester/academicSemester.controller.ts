5eb860481e3dc100debf5ca71421de12e5329660
Springhawk27 <sajjad0mahmud@gmail.com>
2023-06-11 02:32:07 +0600

    try-catch and Api response optimised

--- src/app/modules/academicSemester/academicSemester.controller.ts
+++ src/app/modules/academicSemester/academicSemester.controller.ts
@@ -1,5 +1,7 @@
 import { NextFunction, Request, RequestHandler, Response } from 'express';
+import httpStatus from 'http-status';
 import catchAsync from '../../../shared/catchAsync';
+import sendResponse from '../../../shared/sendResponse';
 import { AcademicSemesterService } from './academicSemester.service';
 
 const createSemester: RequestHandler = catchAsync(
@@ -9,7 +11,13 @@ const createSemester: RequestHandler = catchAsync(
       academicSemesterData
     );
     next();
-    res.status(200).json({
+    // res.status(200).json({
+    //   success: true,
+    //   message: 'Academic Semester is created successfully',
+    //   data: result,
+    // });
+    sendResponse(res, {
+      statusCode: httpStatus.OK,
       success: true,
       message: 'Academic Semester is created successfully',
       data: result,
