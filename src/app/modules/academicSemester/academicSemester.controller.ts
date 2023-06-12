c2dfdc90af6afe80a12b3c58e0107650ae6beb28
Springhawk27 <sajjad0mahmud@gmail.com>
2023-06-13 01:09:39 +0600

    sortBy, sortOrder, limit, search and filter implemented

--- src/app/modules/academicSemester/academicSemester.controller.ts
+++ src/app/modules/academicSemester/academicSemester.controller.ts
@@ -1,7 +1,11 @@
 import { NextFunction, Request, RequestHandler, Response } from 'express';
 import httpStatus from 'http-status';
+import { paginationFields } from '../../../constants/pagination';
 import catchAsync from '../../../shared/catchAsync';
+import pick from '../../../shared/pick';
 import sendResponse from '../../../shared/sendResponse';
+import { academicSemesterFilterableFields } from './academicSemester.constant';
+import { IAcademicSemester } from './academicSemester.interface';
 import { AcademicSemesterService } from './academicSemester.service';
 
 const createSemester: RequestHandler = catchAsync(
@@ -15,7 +19,7 @@ const createSemester: RequestHandler = catchAsync(
     //   message: 'Academic Semester is created successfully',
     //   data: result,
     // });
-    sendResponse(res, {
+    sendResponse<IAcademicSemester>(res, {
       statusCode: httpStatus.OK,
       success: true,
       message: 'Academic Semester is created successfully',
@@ -25,6 +29,56 @@ const createSemester: RequestHandler = catchAsync(
   }
 );
 
+const getAllSemesters = catchAsync(
+  async (req: Request, res: Response, next: NextFunction) => {
+    // const paginationOptions = {
+    //   page: Number(req.query.page),
+    //   limit: Number(req.query.limit),
+    //   sortBy: req.query.sortBy,
+    //   sortOrder: req.query.sortOrder,
+    // };
+
+    // search filter
+    // const filters = pick(req.query, ['searchTerm', 'title', 'code', 'year']);
+    const filters = pick(req.query, academicSemesterFilterableFields);
+    // pagination options
+    const paginationOptions = pick(req.query, paginationFields);
+
+    // console.log(paginationOptions);
+
+    const result = await AcademicSemesterService.getAllSemesters(
+      filters,
+      paginationOptions
+    );
+
+    sendResponse<IAcademicSemester[]>(res, {
+      statusCode: httpStatus.OK,
+      success: true,
+      message: 'Semesters Retrieved Successfully',
+      meta: result.meta,
+      data: result.data,
+    });
+    next();
+  }
+);
+
+const getSingleSemester = catchAsync(
+  async (req: Request, res: Response, next: NextFunction) => {
+    const id = req.params.id;
+    const result = await AcademicSemesterService.getSingleSemester(id);
+
+    sendResponse<IAcademicSemester>(res, {
+      statusCode: httpStatus.OK,
+      success: true,
+      message: 'Semester Retrieved Successfully',
+      data: result,
+    });
+    next();
+  }
+);
+
 export const AcademicSemesterContoller = {
   createSemester,
+  getSingleSemester,
+  getAllSemesters,
 };
--- src/app/modules/user/user.controller.ts
+++ src/app/modules/user/user.controller.ts
@@ -2,6 +2,7 @@ import { NextFunction, Request, RequestHandler, Response } from 'express';
 import httpStatus from 'http-status';
 import catchAsync from '../../../shared/catchAsync';
 import sendResponse from '../../../shared/sendResponse';
+import { IUser } from './user.interface';
 import { UserService } from './user.service';
 
 // const createUser: RequestHandler = async (req, res, next) => {
@@ -28,7 +29,7 @@ const createUser: RequestHandler = catchAsync(
     const { user } = req.body;
     const result = await UserService.createUser(user);
 
-    sendResponse(res, {
+    sendResponse<IUser>(res, {
       statusCode: httpStatus.OK,
       success: true,
       message: 'User created successfully',
