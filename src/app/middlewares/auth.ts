b7569c6bfb25070d9aedcdfbc164869c5c50f235
Springhawk27 <sajjad0mahmud@gmail.com>
2023-06-26 05:06:11 +0600

    authorization and athentication implemented

---
+++ src/app/middlewares/auth.ts
@@ -0,0 +1,37 @@
+import { NextFunction, Request, Response } from 'express';
+import httpStatus from 'http-status';
+import { Secret } from 'jsonwebtoken';
+import config from '../../config';
+import ApiError from '../../errors/ApiErrors';
+import { jwtHelpers } from '../../helpers/jwtHelpers';
+
+const auth =
+  (...requiredRoles: string[]) =>
+  async (req: Request, res: Response, next: NextFunction) => {
+    try {
+      //get authorization token
+      const token = req.headers.authorization;
+      if (!token) {
+        throw new ApiError(httpStatus.UNAUTHORIZED, 'You are not authorized');
+      }
+
+      // athentication - verified or not
+      // verify token
+      let verifiedUser = null;
+
+      verifiedUser = jwtHelpers.verifyToken(token, config.jwt.secret as Secret);
+
+      req.user = verifiedUser; // role , userid
+
+      // athorization - athorized or not(role check)
+      // checking role and permission
+      if (requiredRoles.length && !requiredRoles.includes(verifiedUser.role)) {
+        throw new ApiError(httpStatus.FORBIDDEN, 'Forbidden');
+      }
+      next();
+    } catch (error) {
+      next(error);
+    }
+  };
+
+export default auth;
